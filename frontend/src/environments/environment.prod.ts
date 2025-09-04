9dc7e51214add192dcdad63155e6d9779f57fcfb
alejandrovrod <alejandro.valladares.rodriguez@gmail.com>
2025-09-04 14:09:57 -0600

    se ajusto la variable

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'PLACEHOLDER_API_URL',
+  apiUrl: '{{PLACEHOLDER_API_URL}}/api',
   stripePublishableKey: 'pk_test_your_stripe_key_here',
   features: {
     enableAnalytics: false,
